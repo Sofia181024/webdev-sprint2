936ef20468707a735b5b974783154e3e4faa2cf8
Sofia181024 <sofiagata6000@gmail.com>
2026-05-12 20:07:46 -0300

    Estrutura do Projeto

---
+++ script.js
@@ -0,0 +1,22 @@
+// ESTRUTURA DO PROJETO
+
+const imagemInput =
+    document.getElementById("imagem");
+
+const dataInput =
+    document.getElementById("data");
+
+const horaInput =
+    document.getElementById("hora");
+
+const descricaoInput =
+    document.getElementById("descricao");
+
+const btnSalvar =
+    document.getElementById("btnSalvar");
+
+const resultado =
+    document.getElementById("resultado");
+
+const preview =
+    document.getElementById("preview");
